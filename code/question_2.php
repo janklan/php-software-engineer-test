df08a1aae287956dbb4f648633dd75210fb0c667
Jan Klan <jan@beatee.org>
2016-03-22 13:30:59 +1030

    Answered question_2 task #3

--- code/question_2.php
+++ code/question_2.php
@@ -7,9 +7,38 @@ namespace SoftwareEngineerTest;
  * Class Customer
  * @package SoftwareEngineerTest
  */
+/**
+ * Class Customer
+ * @package SoftwareEngineerTest
+ */
 abstract class Customer {
 
-	/**
+    /**
+     * Bronze level identifier (Customer ID must begin with this letter to indicate a bronze level)
+     */
+    const LEVEL_BRONZE  = 'B';
+
+    /**
+     * Silver level identifier (Customer ID must begin with this letter to indicate a silver level)
+     */
+    const LEVEL_SILVER  = 'S';
+
+    /**
+     * Gold level identifier (Customer ID must begin with this letter to indicate a gold level)
+     */
+    const LEVEL_GOLD    = 'G';
+
+    /**
+     * List of known customer levels and their respective user classes
+     * @var array
+     */
+    private static $allowedLevels = array(
+        self::LEVEL_BRONZE  => '\SoftwareEngineerTest\Bronze_Customer',
+        self::LEVEL_SILVER  => '\SoftwareEngineerTest\Silver_Customer',
+        self::LEVEL_GOLD    => '\SoftwareEngineerTest\Gold_Customer',
+    );
+
+    /**
 	 * @var int Customer ID
      */
 	protected $id;
@@ -48,6 +77,33 @@ abstract class Customer {
     public function deposit($amount) {
         $this->balance += (float)$amount * $this->getBonusCoeficient();
         return $this;
+    }
+
+    /**
+     * Factory method returns the correct customer instance based on the ID specified.
+     *
+     * @see $allowedLevels
+     * @param $id ID in format [A-Z]\d{,9}
+     * @return Descendants of the Customer class
+     * @throws \InvalidArgumentException
+     */
+    public static function get_instance($id) {
+
+        // The first character of the ID should be one of our customer levels
+        $customerLevel = strtoupper($id[0]);
+
+        if (isset(self::$allowedLevels[$customerLevel])) {
+            $customerId = (int)preg_replace('/[^\d]/', '', $id);
+            // Spec says ID and number has maxlength(10), so the (int)$id must be <= 999 999 999
+            //  and > 0, as 0 would mean invalid ID specified
+            if ($customerId > 0 && $customerId <= 999999999) {
+                return new self::$allowedLevels[$customerLevel]($customerId);
+            } else {
+                throw new \InvalidArgumentException('Invalid customer ID specified.');
+            }
+        } else {
+            throw new \InvalidArgumentException('Invalid customer level specified.');
+        }
     }
 
     /**
@@ -96,13 +152,11 @@ class Gold_Customer extends Customer {
 }
 
 $gc = new Gold_Customer(1);
-echo $gc->deposit(100)->get_balance();
+echo Customer::get_instance('G1')->deposit(100)->get_balance();
 echo '<br>';
 
-$gc = new Silver_Customer(2);
-echo $gc->deposit(100)->get_balance();
+echo Customer::get_instance('S2')->deposit(100)->get_balance();
 echo '<br>';
 
-$gc = new Bronze_Customer(3);
-echo $gc->deposit(100)->get_balance();
+echo Customer::get_instance('B3')->deposit(100)->get_balance();
 echo '<br>';
